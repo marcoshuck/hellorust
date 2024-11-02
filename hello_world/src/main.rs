a4725cc5be746001db6717f41229ac74f8a4128e
Marcos Huck <marcos@huck.com.ar>
2024-11-02 12:47:54 -0300

    Improve hello world to use dependencies
    Use ferris says to print a text in the console, but also to play around with different mutable and non-mutable variables
    
    Signed-off-by: Marcos Huck <marcos@huck.com.ar>

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{stdout, BufWriter, StdoutLock};
+use ferris_says::say;
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let mut message = String::from("Hello, world");
+    let mut width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    ferris_print(&mut message, width, &mut writer);
+
+    message = String::from("Hi, this is a new message!");
+    width = message.chars().count();
+    ferris_print(&mut message, width, &mut writer);
+}
+
+fn ferris_print(message: &mut String, width: usize, mut writer: &mut BufWriter<StdoutLock>) {
+    let _ = match say(&message, width, &mut writer) {
+        Ok(_) => println!("All good, input: {}, {}", message, width),
+        Err(error) => println!("An error occurred: {}", error),
+    };
 }
